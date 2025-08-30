c5adfa219c3156f5b94dd62cb084328aeed2391a
bodier123 <bodier123@users.noreply.github.com>
2025-08-30 12:19:29 +0000

    feat: install direnv globally

--- home/dev/common.nix
+++ home/dev/common.nix
@@ -11,6 +11,11 @@
   programs.zsh.enable = true;
   programs.nushell.enable = true;
 
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+
   programs.git = {
     enable = true;
     userName = "bodier123";
